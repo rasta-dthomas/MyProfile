95a5b2b331689b778e2ea8b7b79590d144a3df93
Davin Thomas <dethomas1@gmail.com>
2020-04-15 09:05:55 -0500

    Modified App.js; 1st attempt to change background

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import desktopImage from './Component/public/image/prsnlOfc-desktop.jpg';
+import mobileImage from './Component/public/image/prsnlOfc-desktop.jpg';
 import './App.css';
 
 function App() {
+  const image = window.innerWidth >= 650 ? desktopImage : mobileImage;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundImage: `url(${image})`}}>
+      <div className="App-content">
+        <h1>My Professional Page</h1>
+        <p>Systems/Software Engineer/Full Stack Developer for hire.</p>
+      </div>
     </div>
   );
 }
