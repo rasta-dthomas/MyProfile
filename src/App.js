5492ef997f12ddbcb38b791a9dedf0e2afefcda6
Davin Thomas <dethomas1@gmail.com>
2020-04-16 21:30:23 -0500

    Fixed email link, aligned page elements, added css to cards

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import mobileImage from 'C:/Users/Mashona/MyProfile/my-profile-page/src/Componen
 import Navbar from 'C:/Users/Mashona/MyProfile/my-profile-page/src/Components/Navbar';
 import Card from 'C:/Users/Mashona/MyProfile/my-profile-page/src/Components/Cards';
 import Contact from 'C:/Users/Mashona/MyProfile/my-profile-page/src/Components/Contact';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
@@ -15,18 +16,12 @@ class App extends Component {
       <div className="App" style={{backgroundImage: `url(${image})`}}>
         <Navbar />
         <br />
-        <h1 className="text-color">Davin Thomas</h1>
-          <h3 className="p-color">Systems Engineer/Software Engineer/Full Stack Developer</h3>
-        <div className="App-content">
-          {/* <h1 className="text-color">Davin Thomas</h1>
-          <h3 className="p-color">Systems Engineer/Software Engineer/Full Stack Developer</h3> */}
-        </div>
-        <div>
-          <Card />
+        <Card />
           <hr />
           <br />
+        <h1 className="text-color">Davin Thomas</h1>
+          <h3 className="p-color">Systems Engineer/Software Engineer/Full Stack Developer</h3>
           <Contact />
-        </div>
       </div>
     );
   }
