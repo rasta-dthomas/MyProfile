ca46f037afb0051bda177e248c7de1988a234d6b
Davin Thomas <dethomas1@gmail.com>
2020-04-19 10:37:01 -0500

    Began build ShareLink Contact 4 social media icons

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import contact from 'C:/Users/Mashona/MyProfile/my-profile-page/src/Components/public/image/contactlogo.png';
 import mailme from 'C:/Users/Mashona/MyProfile/my-profile-page/src/Components/public/image/mailmeimgII.png';
+// import ShareLink from 'react-linkedin-share-link';
 import 'C:/Users/Mashona/MyProfile/my-profile-page/src/Components/css/Main.css';
 
 class Contact extends Component {
@@ -8,6 +9,7 @@ class Contact extends Component {
         return (
             <section className="container-1">
                 <img id="contactimg" src={contact} position="center" width="250" height="250" alt="contactlogo" />
+                {/* <ShareLink link='https://' */}
                 <h4>Email Id: dethomas1@gmail.com</h4>
                 <a id="mail" href="https://mail.google.com/mail/?view=cm&fs=1&to=dethomas1@gmail.com"><img id="mailmelogo" src={mailme} alt="mail me" /></a>
                 <h4>Contact Info: (home) 901-512-6902/ (cell) 504-621-4342</h4>
